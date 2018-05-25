bf149d482466f1fdb701428c77697c598a56397c
DamonYT <39619208+DamonYT@users.noreply.github.com>
2018-05-25 11:39:41 +0200

    Add files via upload

---
+++ Hangman- 1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+
+
+
+setup(name= 'Hangman', version= '1.2', py_modules= ['Hangman'], author= 'Damon YT', author_email='damonleigh.dl1@gmail.com', description='A typical game of Hangman, with a cheatcode;)')
+
+
+
